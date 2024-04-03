027a5ed64bfa7040d88c10a93c134bb33dd6b25d
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-04-03 13:09:15 +0300

    barcode

--- src/components/TemplateSmallCard.jsx
+++ src/components/TemplateSmallCard.jsx
@@ -6,25 +6,26 @@ const Template = ({ name, price, discount, barcode }) => {
   return (
     <li className="box small-box">
       <h2>{name}</h2>
-      <p>
-        <span className="price small-price">Ціна:</span>
-        <span className="sum small-sum">{price} грн.</span>
-      </p>
+      <div style={{ display: "flex" }}>
+        <Barcode
+          value={barcode}
+          fontSize={12}
+          height={25}
+          width={1}
+          margin={5}
+          displayValue={false}
+        />
+        <p>
+          <span className="price small-price">Ціна:</span>
+          <span className="sum small-sum">{price} грн.</span>
+        </p>
+      </div>
       <div className="outer-box small-outer-box">
         <div className="inner-box small-inner-box">
           <p>В Неділю!</p>
           <span>-{discount}%</span>
         </div>
         <span>{parseInt(price - (price * discount) / 100)} грн.</span>
-      </div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Barcode
-          value={barcode}
-          fontSize={12}
-          height={25}
-          width={2}
-          margin={5}
-        />
       </div>
     </li>
   );
